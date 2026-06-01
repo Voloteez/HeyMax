32a87e1999edfc5e55f6f278ab08f5167a368eb2
Maxim Durand <durandmaxim7@gmail.com>
2026-06-01 22:02:19 +1000

    Initial Commit

---
+++ heymax/heymaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  heymaxApp.swift
+//  heymax
+//
+//  Created by Maxim Durand on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct heymaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
